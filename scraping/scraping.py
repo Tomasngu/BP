04a5e9805b2b95c4b228613be6e09f206dc60e74
Huu Quy Nguyen <nguyehu7@fit.cvut.cz>
2024-01-22 17:00:30 +0100

    Add docstrings

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,3 +1,4 @@
+from googleapiclient.http import MediaFileUpload
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -17,9 +18,21 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def find_or_create_folder(service, folder_name):
+    """
+    Searches for a folder by name in Google Drive and returns its ID. 
+    If the folder does not exist, it is created.
+
+    Args:
+        service: Authorized Google Drive service instance.
+        folder_name (str): The name of the folder to search or create.
+
+    Returns:
+        str: The ID of the existing or newly created folder.
+    """
     # Search for the folder
     results = service.files().list(
-        q=f"mimeType='application/vnd.google-apps.folder' and name='{folder_name}' and trashed=false",
+        q=f"mimeType='application/vnd.google-apps.folder' and name='{
+            folder_name}' and trashed=false",
         spaces='drive',
         fields='files(id, name)').execute()
     items = results.get('files', [])
@@ -38,54 +51,93 @@ def find_or_create_folder(service, folder_name):
         print(f"Folder created: {folder_name} with ID: {folder.get('id')}")
         return folder.get('id')
 
-from googleapiclient.http import MediaFileUpload
 
 def upload_file(service, file_name, file_path, folder_id, mime_type='image/png'):
+    """
+    Uploads a file to Google Drive under a specific folder.
+
+    Args:
+        service: Authorized Google Drive service instance.
+        file_name (str): The name of the file to be uploaded.
+        file_path (str): The local path of the file to be uploaded.
+        folder_id (str): The ID of the folder in which to upload the file.
+        mime_type (str, optional): The MIME type of the file. Defaults to 'image/png'.
+
+    Returns:
+        None
+    """
     file_metadata = {
         'name': file_name,
         'parents': [folder_id]
     }
     media = MediaFileUpload(file_path, mimetype=mime_type)
-    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    file = service.files().create(body=file_metadata,
+                                  media_body=media, fields='id').execute()
     print(f'File ID: {file.get("id")}')
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive']
 
 
 def auth():
-  """Shows basic usage of the Drive v3 API.
-  Prints the names and ids of the first 10 files the user has access to.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("./token.json"):
-    creds = Credentials.from_authorized_user_file("./token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "./credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())  
-  service = build("drive", "v3", credentials=creds)
+    """
+    Authenticates the user with Google Drive API and returns the service object.
 
-  return service
+    This function handles token creation and refresh for Google Drive API access. 
+    It uses a token.json file for storing user credentials.
+
+    Returns:
+        service: An authorized Google Drive service instance.
+    """
+    creds = None
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("./token.json"):
+        creds = Credentials.from_authorized_user_file("./token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "./credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    service = build("drive", "v3", credentials=creds)
+
+    return service
+
 
 def upload_to_drive(dir, target):
+    """
+    Uploads all files from a specified directory to a Google Drive folder.
+
+    Args:
+        dir (str): The directory containing files to upload.
+        target (str): The name of the target folder in Google Drive.
+
+    Returns:
+        None
+    """
     service = auth()
     folder_id = find_or_create_folder(service, target)
     for filename in os.listdir(dir):
         upload_file(service, filename, os.path.join(dir, filename), folder_id)
 
+
 def scraping(dir):
+    """
+    Performs web scraping using Selenium, saves screenshots of a website, 
+    and stores them in the specified directory.
+
+    Args:
+        dir (str): The directory to save the screenshots.
+
+    Returns:
+        None
+    """
     if os.path.exists(dir):
         shutil.rmtree(dir)
     os.makedirs(dir)
@@ -94,27 +146,28 @@ def scraping(dir):
     current_time = now.strftime("%d_%m__%H_%M")
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--headless") 
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
     chrome_options.add_argument('window-size=1920x1080')
     chrome_options.add_argument('--no-sandbox')
-    
 
     # Set up the Chrome WebDriver with headless option
-    driver = webdriver.Chrome(options=chrome_options)   
+    driver = webdriver.Chrome(options=chrome_options)
     BUTTON_NUM_TO_STREAM = {
-        1:7,
-        2:5,
-        3:6,
-        4:8,
-        5:1,
-        6:2,
-        7:3,
-        8:4}
+        1: 7,
+        2: 5,
+        3: 6,
+        4: 8,
+        5: 1,
+        6: 2,
+        7: 3,
+        8: 4}
     time.sleep(1)
     for i in range(1, 9):
         stream_num = BUTTON_NUM_TO_STREAM[i]
-        url = f'https://www.zoopraha.cz/multimedia/prenos-z-udoli-slonu-zive?cam={stream_num}&res=h&start={stream_num}'
+        url = f'https://www.zoopraha.cz/multimedia/prenos-z-udoli-slonu-zive?cam={
+            stream_num}&res=h&start={stream_num}'
         driver.get(url)
         time.sleep(1)
         driver.save_screenshot('a.png')
@@ -125,25 +178,30 @@ def scraping(dir):
             print('Element not clickable.')
         except NoSuchElementException:
             print('Element not found')
-            
+
         action = ActionChains(driver)
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'playerSLONI')))
-        video_player = driver.find_element(By.ID, 'playerSLONI')  # Replace with the correct ID or selector
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'playerSLONI')))
+        # Replace with the correct ID or selector
+        video_player = driver.find_element(By.ID, 'playerSLONI')
         action.move_to_element(video_player).perform()
         play_button = driver.find_element(By.CLASS_NAME, "jw-icon-playback")
         play_button.click()
 
-        fullscreen_button = driver.find_element(By.CLASS_NAME, "jw-icon-fullscreen")
+        fullscreen_button = driver.find_element(
+            By.CLASS_NAME, "jw-icon-fullscreen")
         fullscreen_button.click()
         time.sleep(1)
         dst = os.path.join(dir, f'screenshot{i}_{current_time}.png')
         driver.save_screenshot(dst)
     driver.quit()
 
+
 def main():
     dir = './images'
     scraping(dir)
     upload_to_drive(dir, 'SLONI')
+
 
 if __name__ == '__main__':
     main()
